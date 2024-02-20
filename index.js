cfec8a9a33134a430ca7f0ac514e743b44965343
Kirill <kiw197204@gmail.com>
2024-02-20 09:32:32 +0300

    feat: спамит сообщение в каналы, куда добавили

--- index.js
+++ index.js
@@ -11,6 +11,71 @@ app.message('hello', async ({ message, say }) => {
   await say(`Hey there <@${message.user}>! ${(await app.client.users.list()).members.map(user => `[${user.name} ${user.is_bot} ${user.team_id}]`).join(', ')}`);
 });
 
+async function publishMessage(id, text) {
+  try {
+    // Call the chat.postMessage method using the built-in WebClient
+    const result = await app.client.chat.postMessage({
+      // The token you used to initialize your app
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: id,
+      text: text
+      // You could also use a blocks[] array to send richer content
+    });
+
+    // Print result, which includes information about the message (like TS)
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+async function spam(message) {
+  try {
+    // Call the conversations.list method using the built-in WebClient
+    const result = await app.client.conversations.list({
+      // The token you used to initialize your app
+      token: process.env.SLACK_BOT_TOKEN
+    });
+
+    for (const channel of result.channels) {
+      if (channel.is_member) {
+        publishMessage(channel.id, message);
+      }
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+spam('KEK');
+
+// Find conversation ID using the conversations.list method
+async function findConversation(name) {
+  try {
+    // Call the conversations.list method using the built-in WebClient
+    const result = await app.client.conversations.list({
+      // The token you used to initialize your app
+      token: process.env.SLACK_BOT_TOKEN
+    });
+    console.log(result);
+
+    for (const channel of result.channels) {
+      if (channel.name === name) {
+        conversationId = channel.id;
+
+        // Print result
+        console.log("Found conversation ID: " + conversationId);
+        // Break from for loop
+        break;
+      }
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
